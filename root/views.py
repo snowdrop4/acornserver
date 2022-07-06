959e1b06e7babb20e358d47671397d593491dc92
snowdrop4 <82846066+snowdrop4@users.noreply.github.com>
2022-07-06 01:26:26 +0100

    fix news ordering

--- root/views.py
+++ root/views.py
@@ -4,6 +4,8 @@ from .models import NewsArticle
 
 
 def homepage(request):
-	articles = NewsArticle.objects.order_by('pub_date')[:5]
+	articles = NewsArticle.objects.order_by('-pub_date')[:5]
+	
+	print(f"pub={articles[0].pub_date}, mod={articles[0].mod_date}")
 	
 	return render(request, 'root/homepage.html', { 'articles': articles })
